81db25fcaad74df01f40265872f334485954228a
ishsta <ishstaglass@yahoo.com>
2022-06-21 01:15:27 -0500

    i hope this works

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+
 
 function App() {
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
+
+const url =`http://api.weatherapi.com/v1/current.json?key=6171b654af8b4afc98c45827222106&q=${location}&aqi=no`
+
+const searchLocation = (event) => {
+  if (event.key === 'Enter') {
+    axios.get(url).then((response) => {
+      setData(response.data)
+      console.log(response.data)
+    })
+    setLocation('')
+  }
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="search">
+        <input 
+        value={location}
+        onChange={event => setLocation(event.target.value)}
+        onKeyPress={searchLocation}
+        placeholder='Enter Location'
+        type="text" />
+      </div>
+      {data.location && data.current != undefined &&
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>{data.location.name}</p>
+          </div>
+          <div className="temp">
+            <h1>{data.current.temp_f}°F</h1>
+          </div>
+          <div className="description">
+            <p>{data.current.condition.text}</p>
+          </div>
+        </div>
+
+          <div className="bottom">
+            <div className="feels">
+              <p>{data.current.feelslike_f}°F</p>
+              <p>Feels like</p>
+            </div>
+            <div className="humidity">
+              <p>{data.current.humidity}%</p>
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              <p>{data.current.wind_mph}mph</p>
+              <p>Wind</p>
+            </div>
+          </div>
+      </div>
+      }
     </div>
   );
 }
